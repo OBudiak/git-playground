70f7368b2179d3a19347dfc866d966d87805e89f
OBudiak <obudiak@kse.org.ua>
2024-10-30 13:39:17 +0200

    lose message

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def congratulate_user():
     print("= Congratulations! You won! =")
     print("=============================")
 
+def lose_user():
+    print("= Next time will be better! =")
 
 def is_game_over():
     return guessed == WORDS_TO_WIN or errors == ERRORS_TO_LOSE
@@ -43,6 +45,8 @@ ERRORS_TO_LOSE = 3
 words = words_fetcher.fetch_words(min_letters=9, max_letters=9)
 full_list = words_fetcher.fetch_words(min_letters=3, max_letters=9)
 word = words[random.randrange(0, len(words))]
+full_list = ['main', 'plan', 'rain', 'train', 'ran']
+word = 'mainplrt'
 
 print(f"Can you make up {WORDS_TO_WIN} words from letters in word provided by me?")
 print(f"Your word is '{word}'")
@@ -69,3 +73,6 @@ while not is_game_over():
     else:
         errors += 1
         print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives more")
+
+if errors == ERRORS_TO_LOSE:
+    lose_user()
